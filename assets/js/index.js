33dd9ffafc87297194fe0e9c432dadba7f9aad53
khuynhgithub <huynh.h.kimberly@gmail.com>
2020-01-22 11:36:56 -0600

    index and css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,9 @@
 let logged_in = false;
 
-
 (function main() {
-    // showCreateUser();
-    fetchBooks();
-    clickListener();
+  // showCreateUser();
+  fetchBooks();
+  clickListener();
 })();
 
 // function showCreateUser() {
@@ -17,7 +16,7 @@ let logged_in = false;
 //     //enter username User.create_or_find_by username
 //     //document.querySelector('#username')
 //     //if logged in, hide form, add logout button
-    
+
 // };
 
 // function submitListener() {
@@ -30,32 +29,31 @@ let logged_in = false;
 //     //login = e.target.value
 // };
 
-
-//list of books or booksearch that try to pull from 
+//list of books or booksearch that try to pull from
 //display list of users
 //book show page from with add/read button
 //user profile page with list of read books
 
 //fetchbooks or search API
 function fetchBooks() {
-    fetch(API.books)
+  fetch(API.books)
     .then(resp => resp.json())
     .then(renderBooks);
-};
+}
 
 function renderBooks(books) {
-    const main = document.querySelector('div.main');
-    const ul = document.createElement('ul');
-    ul.id = "booklist";
-    books.forEach(book => renderBook(book,ul));
-    main.append(ul);
-};
+  const main = document.querySelector("div.main");
+  const ul = document.createElement("ul");
+  ul.id = "booklist";
+  books.forEach(book => renderBook(book, ul));
+  main.append(ul);
+}
 
-function renderBook(book,ul) {
-    const li = document.createElement('li');
-    li.dataset.id = book.id;
-    li.innerHTML = book.title;
-    ul.append(li);
+function renderBook(book, ul) {
+  const li = document.createElement("li");
+  li.dataset.id = book.id;
+  li.innerHTML = book.title;
+  ul.append(li);
 }
 //fetchUsers
 
@@ -63,48 +61,46 @@ function renderBook(book,ul) {
 //Username
 
 function clickListener() {
-
-    
-    document.addEventListener('click',function(e) {
-        //switch case 
-        const click = e.target.parentNode.id
-        switch (click) {
-        case 'booklist':
-            const id = e.target.dataset.id;
-            fetch(API.books + id)
-            .then(resp => resp.json())
-            .then(bookPage);
-            break;
-        };
-    });
-};
+  document.addEventListener("click", function(e) {
+    //switch case
+    const click = e.target.parentNode.id;
+    switch (click) {
+      case "booklist":
+        const id = e.target.dataset.id;
+        fetch(API.books + id)
+          .then(resp => resp.json())
+          .then(bookPage);
+        break;
+    }
+  });
+}
 // //Book Show Page
 function bookPage(book) {
-    const main = document.querySelector('.main');
-    main.firstElementChild.remove();
+  const main = document.querySelector(".main");
+  main.firstElementChild.remove();
 
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.title;
-    
-    const cover = document.createElement('img');
-    cover.src = book.cover_image;
+  const h2 = document.createElement("h2");
+  h2.innerHTML = book.title;
 
-    const h3 = document.createElement('h3');
-    h3.innerHTML = book.author;
+  const cover = document.createElement("img");
+  cover.src = book.cover_image;
 
-    const h4 = document.createElement('h4');
-    h4.innerHTML = book.publisher;
+  const h3 = document.createElement("h3");
+  h3.innerHTML = book.author;
 
-    const desc = document.createElement('p');
-    desc.innerHTML = book.description;
+  const h4 = document.createElement("h4");
+  h4.innerHTML = book.publisher;
 
-    const pages = document.createElement('p');
-    pages.innerHTML = `${book.page_count} pages`;
+  const desc = document.createElement("p");
+  desc.innerHTML = book.description;
 
-    const genre = document.createElement('p');
-    genre.innerHTML = `Genre: ${book.genre}`;
+  const pages = document.createElement("p");
+  pages.innerHTML = `${book.page_count} pages`;
 
-    main.append(h2,cover,h3,h4,desc,pages,genre);
+  const genre = document.createElement("p");
+  genre.innerHTML = `Genre: ${book.genre}`;
 
-    //needs add/read button, only shown if logged_in
-};
+  main.append(h2, cover, h3, h4, desc, pages, genre);
+
+  //needs add/read button, only shown if logged_in
+}
