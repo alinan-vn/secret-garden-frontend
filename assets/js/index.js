f4b33cc02037e1d4a05aa1ebc55c41e45a057789
hfarnsworth <havilahfarnsworth@gmail.com>
2020-01-23 10:37:58 -0600

    user login progress

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,49 +9,49 @@ let current_user_books = [];
 
 
 function removeChildElementsFor(parentElement) {
-    let childElement;
-    while (childElement = parentElement.lastElementChild) {
-        parentElement.removeChild(childElement);
-    };
+  let childElement;
+  while (childElement = parentElement.lastElementChild) {
+    parentElement.removeChild(childElement);
+  };
 };
 
 function fetchUsers() {
-    fetch(API.users)
-    .then(resp => resp.json())
-    .then(renderUsers);
+  fetch(API.users)
+  .then(resp => resp.json())
+  .then(renderUsers);
 };
 
 function renderUsers(users) {
-    const main = document.querySelector('main');
-    removeChildElementsFor(main);
+  const main = document.querySelector('main');
+  removeChildElementsFor(main);
 
-    const ul = document.createElement("ul");
-    users.forEach(user => renderUser(user, ul));
-    main.append(ul);
+  const ul = document.createElement("ul");
+  users.forEach(user => renderUser(user, ul));
+  main.append(ul);
 };
 
 function renderUser(user, ul) {
-    const li = document.createElement('li');
-    li.dataset.id = user.id;
-    li.innerHTML = `${user.username}'s Bookcase`;
-    li.className = 'userlist';
-    ul.append(li);
+  const li = document.createElement('li');
+  li.dataset.id = user.id;
+  li.innerHTML = `${user.username}'s Bookcase`;
+  li.className = 'userlist';
+  ul.append(li);
 };
 
 //fetchbooks or search API
 function fetchBooks() {
-    fetch(API.books)
-    .then(resp => resp.json())
-    .then(renderBooks);
+  fetch(API.books)
+  .then(resp => resp.json())
+  .then(renderBooks);
 };
 
 function renderBooks(books) {
-    const main = document.querySelector('main');
-    removeChildElementsFor(main);
+  const main = document.querySelector('main');
+  removeChildElementsFor(main);
 
-    const ul = document.createElement("ul");
-    books.forEach(book => renderBook(book, ul));
-    main.append(ul);
+  const ul = document.createElement("ul");
+  books.forEach(book => renderBook(book, ul));
+  main.append(ul);
 };
 
 function renderBook(book, ul) {
@@ -67,116 +67,138 @@ function renderBook(book, ul) {
 //Username
 
 function clickListener() {
-    document.addEventListener('click',function(e) {
-        const click = e.target.className;
-        
-        switch (click) {
-        case 'booklist':
-            const bookId = e.target.dataset.id;
-            fetch(API.books + bookId)
-            .then(resp => resp.json())
-            .then(bookPage);
-            break;
-        case 'userlist':
-            const userId = e.target.dataset.id;
-            fetch(API.users + userId)
-            .then(resp => resp.json())
-            .then(userPage);
-            break;
-        case 'add-button':
-            const bookAdd = e.target.dataset.bookId;
-            postBookcase(bookAdd);
-            break;
-        case 'books-index':
-            fetchBooks();
-            break;
-        case 'signin':
-            const loginDiv = document.querySelector('.login-wrap');
-            loginDiv.classList.toggle('hide');
-            break;
-        case 'users-index':
-            fetchUsers();
-            break;
-        case 'button signin':
-            console.log(e.target.parentNode.previousElementSibling.lastElementChild.value);
-            e.target.parentNode.previousElementSibling.lastElementChild.value = '';
-            break;
-        case 'button signup':
-            console.log(e.target.parentNode.previousElementSibling.lastElementChild.value);
-            e.target.parentNode.previousElementSibling.lastElementChild.value = '';
-            break;
-        };
-    });
+  document.addEventListener('click',function(e) {
+    const click = e.target.className;
+    
+    switch (click) {
+    case 'booklist':
+      const bookId = e.target.dataset.id;
+      fetch(API.books + bookId)
+      .then(resp => resp.json())
+      .then(bookPage);
+      break;
+    case 'userlist':
+      const userId = e.target.dataset.id;
+      fetch(API.users + userId)
+      .then(resp => resp.json())
+      .then(userPage);
+      break;
+    case 'add-button':
+      const bookAdd = e.target.dataset.bookId;
+      postBookcase(bookAdd);
+      break;
+    case 'books-index':
+      fetchBooks();
+      break;
+    case 'signin':
+      const loginDiv = document.querySelector('.login-wrap');
+      loginDiv.classList.toggle('hide');
+      break;
+    case 'users-index':
+      fetchUsers();
+      break;
+    case 'button signin':
+      const uName = e.target.parentNode.previousElementSibling.lastElementChild.value;
+      e.target.parentNode.previousElementSibling.lastElementChild.value = '';
+      loginUser(uName);
+      break;
+    case 'button signup':
+      console.log(e.target.parentNode.previousElementSibling.lastElementChild.value);
+      e.target.parentNode.previousElementSibling.lastElementChild.value = '';
+      
+      break;
+    };
+  });
 };
 
+function loginUser(uName) {
+  fetch(API.users)
+  .then(resp => resp.json())
+  .then(users => findUser(users,uName));
+};
+
+function findUser(users,uName) {
+  if (users.find(user => user.username.toLowerCase() === uName.toLowerCase())) {
+    const user = users.find(user => user.username.toLowerCase() === uName.toLowerCase())
+    current_user = user.id;
+    logged_in = true;
+    const loginDiv = document.querySelector('.login-wrap');
+    loginDiv.classList.toggle('hide');
+    const signin = document.querySelector('.signin');
+    signin.classList.toggle('hide');
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `Welcome, ${user.username}<button class="signout">Sign Out</button>`;
+  };
+}
+
 function postBookcase(bookId) {
-    const reqObj = {
-        "method": "POST",
-        "headers": API.headers,
-        "body": JSON.stringify(
-            {
-                "user_id": current_user,
-                "book_id": bookId
-            }
-        )
-    };
-    fetch(API.bookcases,reqObj)
-    .then(resp => resp.json())
-    .then();
+  const reqObj = {
+    "method": "POST",
+    "headers": API.headers,
+    "body": JSON.stringify(
+      {
+        "user_id": current_user,
+        "book_id": bookId
+      }
+    )
+  };
+  fetch(API.bookcases,reqObj)
+  .then(resp => resp.json())
+  .then();
 };
 
 
 function userPage(user) {
-    const main = document.querySelector('main');
-    removeChildElementsFor(main);
+  const main = document.querySelector('main');
+  removeChildElementsFor(main);
 
-    const h2 = document.createElement('h2');
-    h2.innerHTML = `${user.username}'s Bookcase`;
+  const h2 = document.createElement('h2');
+  h2.innerHTML = `${user.username}'s Bookcase`;
 
-    const ul = document.createElement("ul");
-    user.books.forEach(book => renderBook(book, ul));
-    const userBooks = ul.children;
-    const rmvBtn = document.createElement('button');
-    userBooks.forEach(book => book.append(rmvBtn));
-    main.append(h2,ul);
+  const ul = document.createElement("ul");
+  user.books.forEach(book => renderBook(book, ul));
+  const userBooks = ul.children;
+  const rmvBtn = document.createElement('button');
+  userBooks.forEach(book => book.append(rmvBtn));
+  main.append(h2,ul);
 };
 
 // //Book Show Page
 function bookPage(book) {
-    let bookId = book.id;
-    const main = document.querySelector('main');
-    removeChildElementsFor(main);
+  let bookId = book.id;
+  const main = document.querySelector('main');
+  removeChildElementsFor(main);
 
-    const h2 = document.createElement("h2");
-    h2.innerHTML = book.title;
+  const h2 = document.createElement("h2");
+  h2.innerHTML = book.title;
 
-    const cover = document.createElement("img");
-    cover.src = book.cover_image;
+  const cover = document.createElement("img");
+  cover.src = book.cover_image;
 
-    const h3 = document.createElement("h3");
-    h3.innerHTML = book.author;
+  const h3 = document.createElement("h3");
+  h3.innerHTML = book.author;
 
-    const h4 = document.createElement("h4");
-    h4.innerHTML = book.publisher;
+  const h4 = document.createElement("h4");
+  h4.innerHTML = book.publisher;
 
-    const desc = document.createElement("p");
-    desc.innerHTML = book.description;
+  const desc = document.createElement("p");
+  desc.innerHTML = book.description;
 
-    const pages = document.createElement("p");
-    pages.innerHTML = `${book.page_count} pages`;
+  const pages = document.createElement("p");
+  pages.innerHTML = `${book.page_count} pages`;
 
-    const genre = document.createElement("p");
-    genre.innerHTML = `Genre: ${book.genre}`;
+  const genre = document.createElement("p");
+  genre.innerHTML = `Genre: ${book.genre}`;
 
-    main.append(h2,cover,h3,h4,desc,pages,genre);
-    const addBtn = document.createElement('button');
-    addBtn.innerText = "add to Bookcase";
-    addBtn.className = "add-button";
-    addBtn.dataset.bookId = bookId;
-    main.append(addBtn);
+  main.append(h2,cover,h3,h4,desc,pages,genre);
+  const addBtn = document.createElement('button');
+  addBtn.innerText = "add to Bookcase";
+  addBtn.className = "add-button";
+  addBtn.dataset.bookId = bookId;
+  main.append(addBtn);
 
-    // needs add/read button, only shown if logged_in
-    if (!logged_in) {
-        addBtn.classList.toggle('hide');
-    };
+  // needs add/read button, only shown if logged_in
+  if (!logged_in) {
+      addBtn.classList.toggle('hide');
+  };
 };
